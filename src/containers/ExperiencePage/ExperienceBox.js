8a96ae270cc446ab130b303fe298cbbe8411300a
gitVaibhav1999 <vaibhavsanjaymishra@gmail.com>
2022-11-06 21:34:23 +0530

    created experience divider

--- src/containers/ExperiencePage/ExperienceBox.js
+++ src/containers/ExperiencePage/ExperienceBox.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-export const ExperienceBox = ({data}) => {
+export const ExperienceBox = ({ data }) => {
   const { duration, company, title } = data;
-    // debugger
+  // debugger
   return (
-    <div className="experienceBox flex-column text-left">
-      <span className="bold">{title}</span>
-      <span>{company}</span>
-      <span className="hand-written font-m">{duration}</span>
+    <div className="experienceBox flex text-left">
+      <span className="experienceBox__divider">
+        <div className="experienceBox__divider__circle"></div>
+        <div className="experienceBox__divider__line"></div>
+      </span>
+      <span className="experienceBox__content flex-column">
+        <span className="bold font-m">{title}</span>
+        <span>{company}</span>
+        <span className="hand-written">{duration}</span>
+      </span>
     </div>
   );
 };
